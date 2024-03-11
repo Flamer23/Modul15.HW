60c92b9dfc9eebb2e840daecf023b10e427ba335
Илья Зуев <zueviliya777@gmail.com>
2024-03-11 16:08:02 +0300

    Добавьте файлы проекта.

---
+++ Modul15.HW/Classroom.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul15.HW
+{
+    public class Classroom
+    {
+        public List<string> Students = new List<string>();
+    }
+}
---
+++ Modul15.HW/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Modul15.HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+            };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var allStudents = classes.SelectMany(cl => cl.Students).ToArray();
+
+
+            return allStudents;
+        }
+    }
+}
